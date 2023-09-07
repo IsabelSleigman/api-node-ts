f29674504896d86dc15c3205ba10283572f73688
isabel.sleigman <sleimusic@gmail.com>
2023-09-07 19:24:29 -0300

    Feat: Iniciando o servidor, criando rotas get e post teste

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import express, { Request, Response } from 'express';
+
+const porta: number = 5000
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (request: Request, response: Response) => {
+    return response.status(200).json({message: 'Servidor rodando'})
+})
+
+server.post('/user', (request: Request, response: Response) => {
+    const body = request.body
+    console.log(body)
+    return response.status(201).json({message: 'Usuário criado com sucesso'})
+})
+
+server.listen(porta,  () => console.log(`Servidor rodando http://localhost:${porta}.`))
