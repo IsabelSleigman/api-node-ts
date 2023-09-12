b4cb767a35723c6a7a6cdf13eade4f5b34d8676e
isabel.sleigman <sleimusic@gmail.com>
2023-09-12 19:52:57 -0300

    Feat: configurando banco de dados, criando scripts

---
+++ src/database/migrations/1694559129892-User.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class User1694559129892 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import 'reflect-metadata'
 import express, { Request, Response } from 'express';
 import { router } from './routes';
+import { AppDataSource } from './database';
 const porta: number = 5000
 
 const server = express();
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Conectado ao banco de dados!")
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 
 server.use(express.json())
 server.use(router)
